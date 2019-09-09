ae392d165b766e7456a29fa1d4caff7180dd4eb8
Abhishek K <abhishek.kumar.jec@gmail.com>
2019-09-09 23:52:12 +0530

    stack using single queue

--- src/com/challenge/StackUsingSingleQueue.java
+++ src/com/challenge/StackUsingSingleQueue.java
@@ -3,36 +3,34 @@ package com.challenge;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class StackUsingDoubleQueue<E> {
+public class StackUsingSingleQueue<E> {
     private Queue<E> q1 = new LinkedList<>();
-    private Queue<E> q2 = new LinkedList<>();
+
     private int size;
 
-    public void push(E element) {
-        q2.add(element);
-        while (!q1.isEmpty()) {
-            q2.add(q1.remove());
-        }
-
-        Queue<E> q = q1;
-        q1 = q2;
-        q2 = q;
-        size++;
-    }
-
-    public E pop(){
-        size--;
-        return q1.remove();
-    }
-
-    public E peek(){
-        return q1.peek();
+    public int size() {
+        return size;
     }
 
     public boolean isEmpty() {
         return size == 0;
     }
 
+    public void push(E element) {
+        q1.add(element);
+        for (int i =0; i< size; i++){
+            q1.add(q1.remove());
+        }
+        size++;
+    }
+
+    public E pop() {
+        size--;
+        return q1.remove();
+    }
+}
+
+class TestStackUsingSingleQueue{
     public static void main(String[] args) {
         System.out.println("------------------------");
         StackUsingDoubleQueue<Integer> q3 = new StackUsingDoubleQueue<>();
@@ -42,9 +40,10 @@ public class StackUsingDoubleQueue<E> {
         q3.push(15);
         System.out.println(q3.pop());
         System.out.println(q3.pop());
+        q3.push(16);
+        q3.push(17);
+        System.out.println(q3.pop());
         System.out.println(q3.pop());
         System.out.println("------------------------");
     }
-
-
 }
