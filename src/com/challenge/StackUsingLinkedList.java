019ed1ba225f1f22fd913dba704d284b936fe9aa
Abhishek K <abhishek.kumar.jec@gmail.com>
2019-09-12 12:00:37 +0530

    stack using linked list

---
+++ src/com/challenge/StackUsingLinkedList.java
@@ -0,0 +1,68 @@
+package com.challenge;
+
+public class StackUsingLinkedList<E> {
+
+    private Node<E> first;
+
+    private int size;
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public void push(E element) {
+        if(first == null) {
+            first = new Node<>(element);
+        } else {
+            Node<E> oldFirstNode = first;
+            first = new Node<>(element);
+            first.next = oldFirstNode;
+        }
+        size++;
+    }
+
+    public E pop() {
+        if(first == null){
+            return null;
+        } else {
+            Node<E> oldNode = first;
+            E ele = oldNode.element;
+            first = first.next;
+            oldNode = null;
+            return ele;
+        }
+    }
+
+    static class Node<E> {
+        E element;
+        Node<E> next;
+
+        public Node(E element){
+            this.element = element;
+        }
+    }
+
+}
+
+class StackUsingLinkedListTest{
+
+    public static void main(String[] args) {
+        System.out.println("------------------------");
+        StackUsingLinkedList<Integer> q3 = new StackUsingLinkedList<>();
+        q3.push(12);
+        q3.push(13);
+        q3.push(14);
+        q3.push(15);
+        System.out.println(q3.pop());
+        System.out.println(q3.pop());
+        q3.push(16);
+        q3.push(17);
+        System.out.println(q3.pop());
+        System.out.println(q3.pop());
+        System.out.println("------------------------");
+    }
+}
